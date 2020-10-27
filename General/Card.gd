46f330c2512a2f6310e049ff4dcd989a873457a7
Allen Davis <allendaviskoonan@gmail.com>
2020-10-27 13:18:51 +0530

    Added changable values for color and number for card

--- General/Card.gd
+++ General/Card.gd
@@ -1,11 +1,38 @@
 extends CenterContainer
 class_name Card
 
+enum Colors {
+	RED,
+	YELLOW,
+	GREEN,
+	BLUE
+}
+
+export var number := 0
+export (Colors) var color
 var is_being_dragged := false
+
+onready var colorRect: ColorRect = $ColorRect
+onready var numberLabel: Label = $NumberLabel
 
 
 func _ready() -> void:
 	connect("gui_input", self, "_on_gui_input")
+	display_correct_color()
+	numberLabel.text = str(number)
+
+func display_correct_color() -> void:
+	match color:
+		Colors.RED:
+			colorRect.color = Color.red
+		Colors.BLUE:
+			colorRect.color = Color.blue
+		Colors.YELLOW:
+			colorRect.color = Color.yellow
+		Colors.GREEN:
+			colorRect.color = Color.green
+		_:
+			colorRect.color = Color.white
 
 
 func _process(delta: float) -> void:
