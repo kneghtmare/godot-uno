c74e1430abf4d818fc4cc8377e8495a35c14f7e5
Allen Davis <allendaviskoonan@gmail.com>
2020-10-27 17:05:18 +0530

    Cards are now resources.

--- Cards/Card.gd
+++ Cards/Card.gd
@@ -1,55 +1,5 @@
-extends CenterContainer
-class_name CardDisplay
+extends Resource
+class_name Card
 
-enum Colors {
-	RED,
-	YELLOW,
-	GREEN,
-	BLUE
-}
-
-export var number := 0
-export (Colors) var color
-var is_being_dragged := false
-
-onready var colorRect: ColorRect = $ColorRect
-onready var numberLabel: Label = $NumberLabel
-
-
-func _ready() -> void:
-	update_display()
-
-
-func _process(delta: float) -> void:
-	if is_being_dragged:
-		rect_position = get_global_mouse_position()
-
-
-#displays proper color that was exported
-func update_display() -> void:
-	match color:
-		Colors.RED:
-			colorRect.color = Color.red
-		Colors.BLUE:
-			colorRect.color = Color.blue
-		Colors.YELLOW:
-			colorRect.color = Color.yellow
-		Colors.GREEN:
-			colorRect.color = Color.green
-		_: #if color is somehow none of the above values, set color to white
-			colorRect.color = Color.white
-			
-	numberLabel.text = str(number)
-	
-
-func get_drag_data(_position: Vector2) -> Dictionary:
-	var data := {}
-	data.number = number
-	data.color_rect_color = colorRect.color
-	data.color = color
-	data.card = self
-	var dragPreview := ColorRect.new()
-	dragPreview.rect_size = colorRect.rect_size
-	dragPreview.color = colorRect.color
-	set_drag_preview(dragPreview)
-	return data
+var number := 0
+var color
--- Cards/DrawPileCard.gd
+++ Cards/DrawPileCard.gd
@@ -18,6 +18,7 @@ var cardScene := preload("res://Cards/Card.tscn")
 var available_colors = [Colors.RED, Colors.YELLOW, Colors.GREEN, Colors.BLUE]
 var available_numbers = [0,1,2,3,4,5,6,7,8,9]
 var done_cards := []
+var card := Card.new()
 
 onready var colorRect := $ColorRect
 onready var numberLabel := $NumberLabel
@@ -25,13 +26,13 @@ onready var cardGridContainer = get_node(card_grid_container_path)
 
 
 func _ready() -> void:
-	change_card()
-	update_card()
+	card = make_new_card()
+	update_display()
 	connect("gui_input", self, "_on_gui_input")
 	
 
-func update_card() -> void:
-	match color:
+func update_display() -> void:
+	match card.color:
 		Colors.RED:
 			colorRect.color = Color.red
 		Colors.BLUE:
@@ -43,30 +44,27 @@ func update_card() -> void:
 		_: #if color is somehow none of the above values, set color to white
 			colorRect.color = Color.white
 	
-	numberLabel.text = str(number)
+	numberLabel.text = str(card.number)
 	
 
 func _on_gui_input(event: InputEvent):
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_index == BUTTON_LEFT:
 			var newCard = cardScene.instance()
-			newCard.number = self.number
-			newCard.color = self.color
+			newCard.number = card.number
+			newCard.color = card.color
 			cardGridContainer.add_child(newCard)
-			change_card()
-			update_card()
+			card = make_new_card()
+			update_display()
 			
 
-func change_card() -> void:
+func make_new_card() -> Card:
+	var card = Card.new()
 	var random_color = available_colors[randi() % available_colors.size()]
 	var random_number = available_numbers[randi() % available_numbers.size()]
 	
-	color = random_color
-	number = random_number
+	card.color = random_color
+	card.number = random_number
 	
-	var card_data := {
-		"number": random_number,
-		"color" : random_color
-	}
-	
-	done_cards.append(card_data)
+	done_cards.append(card)
+	return card
