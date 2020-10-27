4cf993e09b382b2d7f15c9aa58cf7a18ca0abb65
Allen Davis <allendaviskoonan@gmail.com>
2020-10-27 13:06:29 +0530

    Added proper whitespaces

--- General/Card.gd
+++ General/Card.gd
@@ -3,12 +3,15 @@ class_name Card
 
 var is_being_dragged := false
 
+
 func _ready() -> void:
 	connect("gui_input", self, "_on_gui_input")
+
 
 func _process(delta: float) -> void:
 	if is_being_dragged:
 		rect_position = get_global_mouse_position()
+
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event.is_action_pressed("drag"):
